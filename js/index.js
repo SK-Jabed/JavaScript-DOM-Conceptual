2c299eb04ca24bfbac8135a3def736c72bcd1168
Sheikh Jabed <jabedbd2295@gmail.com>
2024-09-17 21:49:52 +0600

    Index HTML and JS Files Added

---
+++ js/index.js
@@ -0,0 +1,24 @@
+/**
+ * Single Element Selector
+ */
+const header = document.getElementById("header");
+header.style.color = "tomato";
+
+const uniquePara = document.querySelector("unique-para");
+uniquePara.style.border = "2px solid red";
+
+/**
+ * Multiple Element Selector
+ */
+const myTags = document.getElementsByTagName("p");
+for (let i = 0; i < myTags.length; i++) {
+    const element = myTags[1];
+    element.style.backgroundColor = "Yellow";
+    element.style.fontSize = "24px";
+}
+
+const allClassElements = document.getElementsByClassName("info");
+
+
+const myParagraph = document.querySelectorAll(".info");
+
